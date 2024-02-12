bfc28790241750cf590d99d45e59ce6f6f1f9764
Trevor Augustine <122263629+TrevorA17@users.noreply.github.com>
2024-02-12 12:55:26 +0300

    Mean, mode, median

--- ExploratoryDataAnalysis.R
+++ ExploratoryDataAnalysis.R
@@ -16,5 +16,47 @@ iris_data$species <- factor(iris_data$species, levels = species_levels)
 # Display the dataset
 View(iris_data)
 
+# Assuming iris_data is the name of your dataset
+
+# Calculate measures of central tendency for sepal_length
+mean_sepal_length <- mean(iris_data$sepal_length)
+median_sepal_length <- median(iris_data$sepal_length)
+mode_sepal_length <- Mode(iris_data$sepal_length)
+
+cat("Measures of Central Tendency for sepal_length:\n")
+cat(paste0("Mean: ", round(mean_sepal_length, 2)), "\n")
+cat(paste0("Median: ", median_sepal_length), "\n")
+cat(paste0("Mode: ", mode_sepal_length), "\n\n")
+
+# Calculate measures of central tendency for sepal_width
+mean_sepal_width <- mean(iris_data$sepal_width)
+median_sepal_width <- median(iris_data$sepal_width)
+mode_sepal_width <- Mode(iris_data$sepal_width)
+
+cat("Measures of Central Tendency for sepal_width:\n")
+cat(paste0("Mean: ", round(mean_sepal_width, 2)), "\n")
+cat(paste0("Median: ", median_sepal_width), "\n")
+cat(paste0("Mode: ", mode_sepal_width), "\n\n")
+
+# Calculate measures of central tendency for petal_length
+mean_petal_length <- mean(iris_data$petal_length)
+median_petal_length <- median(iris_data$petal_length)
+mode_petal_length <- Mode(iris_data$petal_length)
+
+cat("Measures of Central Tendency for petal_length:\n")
+cat(paste0("Mean: ", round(mean_petal_length, 2)), "\n")
+cat(paste0("Median: ", median_petal_length), "\n")
+cat(paste0("Mode: ", mode_petal_length), "\n\n")
+
+# Calculate measures of central tendency for petal_width
+mean_petal_width <- mean(iris_data$petal_width)
+median_petal_width <- median(iris_data$petal_width)
+mode_petal_width <- Mode(iris_data$petal_width)
+
+cat("Measures of Central Tendency for petal_width:\n")
+cat(paste0("Mean: ", round(mean_petal_width, 2)), "\n")
+cat(paste0("Median: ", median_petal_width), "\n")
+cat(paste0("Mode: ", mode_petal_width), "\n\n")
+
 
 
